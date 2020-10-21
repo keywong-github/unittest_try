8947ba75fff046892a9bdd52b2c47c49511aa9ee
keywong-github <907740164@qq.com>
2020-10-21 12:28:13 +0800

    111

--- test_case/test_baidu_login.py
+++ test_case/test_baidu_login.py
@@ -2,19 +2,18 @@ import unittest
 import sys
 from os.path import dirname,abspath
 sys.path.append(dirname(dirname(abspath(__file__))))
-from common.base_page import BasePage
 from action.login_action import LoginAction
 from ddt import ddt,data,unpack
 from selenium import webdriver
 
 @ddt
 class Test_Login(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls.driver=webdriver.Chrome()
-    @classmethod
-    def tearDownClass(cls):
-        cls.driver.quit()
+
+    def setUp(self):
+        self.driver=webdriver.Chrome()
+
+    def tearDown(self):
+        self.driver.quit()
 
     @data("https://www.baidu.com/")
     #@unpack
